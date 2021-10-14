2736801583cce34d353281c3fb10ca35879575be
Benjamin <eunvanz@naver.com>
2021-10-14 21:43:52 +0900

    chore: exclude stories and tests

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,6 +4,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-type Optional<T extends object, K extends keyof T = keyof T> = Omit<T, K> &
-  Partial<Pick<T, K>>;
