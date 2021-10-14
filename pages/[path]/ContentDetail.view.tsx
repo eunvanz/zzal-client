99ba8cc9f5f48555ee1fba9c71f1ad6a33c1076a
Benjamin <eunvanz@naver.com>
2021-10-15 00:59:55 +0900

    feat: add another meme button

--- pages/[path]/ContentDetail.view.tsx
+++ pages/[path]/ContentDetail.view.tsx
@@ -1,4 +1,5 @@
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Box, Typography, Button } from "@mui/material";
+import { useRouter } from "next/dist/client/router";
 import { Content } from "~/types";
 
 export interface ContentDetailProps {
@@ -6,6 +7,8 @@ export interface ContentDetailProps {
 }
 
 const ContentDetail: React.FC<ContentDetailProps> = ({ content }) => {
+  const router = useRouter();
+
   return (
     <Container
       maxWidth="sm"
@@ -37,6 +40,15 @@ const ContentDetail: React.FC<ContentDetailProps> = ({ content }) => {
           {content.title}
         </Typography>
         <Typography variant="body1">{content.description}</Typography>
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "flex-end", mt: 2 }}>
+          <Button
+            size="small"
+            variant="outlined"
+            onClick={() => router.push("/registration")}
+          >
+            다른 짤 등록하기
+          </Button>
+        </Box>
       </Box>
     </Container>
   );
