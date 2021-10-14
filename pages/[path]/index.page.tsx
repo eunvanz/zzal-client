5163cb27ce9ce8a8a40bcef4a82ca6962290e505
Benjamin <eunvanz@naver.com>
2021-10-14 23:32:59 +0900

    feat: custom title and description

--- pages/[path]/index.page.tsx
+++ pages/[path]/index.page.tsx
@@ -1,4 +1,5 @@
 import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
 import api from "~/api";
 import { DEFAULT_TITLE } from "~/constants/text";
@@ -12,12 +13,18 @@ export interface ContentDetailPageProps {
 export const CommonContentDetail = ({ content }: ContentDetailPageProps) => {
   const image = content.images[0];
 
+  const router = useRouter();
+  const { t, d } = router.query as { t: string; d: string };
+
   return (
     <>
       <Head>
-        <title>{content.title || DEFAULT_TITLE}</title>
-        {content.description && (
-          <meta property="og:description" content={content.description} />
+        <title>{t || content.title || DEFAULT_TITLE}</title>
+        {(d || content.description) && (
+          <meta
+            property="og:description"
+            content={d || content.description || undefined}
+          />
         )}
         <meta property="og:image" content={image.url} />
         <meta property="og:image:url" content={image.url} />
