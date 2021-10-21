c4305e25ac82397043b4c38007e833a758084456
Benjamin <eunvanz@naver.com>
2021-10-22 07:59:52 +0900

    fix: fix not found page

--- pages/not-found.page.tsx
+++ pages/not-found.page.tsx
@@ -1,18 +1,19 @@
 import Head from "next/head";
 import Link from "next/link";
+import ROUTES from "~/routes";
 
 const NotFoundPage: React.FC<void> = () => {
   return (
     <>
       <Head>
         <title>zzal.me - 등록되지 않은 짤입니다.</title>
-        <meta name="description" content="손쉽게 짤 링크를 생성하고 공유하세요." />
+        <meta name="og:description" content="짤을 등록 후 사용해주세요." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h2>등록되지 않은 짤입니다.</h2>
-        <Link href="/registration">짤 등록하러 가기</Link>
+        <Link href={ROUTES.REGISTRATION__NEW}>짤 등록하러 가기</Link>
       </main>
     </>
   );
