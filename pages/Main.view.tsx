ff94acd0d1cfa1ce183da37c182964673e5c4b52
Benjamin <eunvanz@naver.com>
2021-10-23 21:40:10 +0900

    feat: add order select

--- pages/Main.view.tsx
+++ pages/Main.view.tsx
@@ -1,19 +1,39 @@
-import { Container } from "@mui/material";
+import { Container, Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import BaseLayout from "~/components/BaseLayout";
 import ContentList from "~/components/ContentList";
 import Intersection from "~/components/Intersection";
-import { Content } from "~/types";
+import { Content, CONTENT_ORDER } from "~/types";
 
 export interface MainProps {
   contents?: Content[];
   onFetchNextPage: VoidFunction;
   hasNextPage?: boolean;
+  onChangeOrder: (order: CONTENT_ORDER) => void;
+  order: CONTENT_ORDER;
 }
 
-const Main: React.FC<MainProps> = ({ contents, onFetchNextPage, hasNextPage }) => {
+const Main: React.FC<MainProps> = ({
+  contents,
+  onFetchNextPage,
+  hasNextPage,
+  onChangeOrder,
+  order,
+}) => {
   return (
     <BaseLayout>
       <Container sx={{ py: 2 }}>
+        <Box sx={{ mb: 2, width: "100%" }}>
+          <FormControl variant="standard">
+            <Select
+              value={order}
+              onChange={(e) => onChangeOrder(e.target.value as CONTENT_ORDER)}
+              label
+            >
+              <MenuItem value={CONTENT_ORDER.POPULARITY}>인기순</MenuItem>
+              <MenuItem value={CONTENT_ORDER.LATEST}>최신순</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
         {contents && <ContentList contents={contents} />}
         {hasNextPage && <Intersection onIntersect={onFetchNextPage} />}
       </Container>
--- pages/useMainProps.ts
+++ pages/useMainProps.ts
@@ -1,22 +1,32 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getMergedPageData } from "~/helpers/projectHelpers";
 import useContentListQuery from "~/queries/useContentListQuery";
 import { CONTENT_ORDER } from "~/types";
 import { MainProps } from "./Main.view";
 
 const useMainProps: () => MainProps = () => {
-  const { data, fetchNextPage, hasNextPage } = useContentListQuery({
-    orderBy: CONTENT_ORDER.POPULARITY,
+  const [order, setOrder] = useState(CONTENT_ORDER.POPULARITY);
+
+  const { data, fetchNextPage, hasNextPage, refetch } = useContentListQuery({
+    orderBy: order,
   });
 
   const contents = useMemo(() => {
     return data ? getMergedPageData(data.pages) : undefined;
   }, [data]);
 
+  useEffect(() => {
+    if (order) {
+      refetch();
+    }
+  }, [order, refetch]);
+
   return {
     contents,
     onFetchNextPage: fetchNextPage,
     hasNextPage,
+    order,
+    onChangeOrder: setOrder,
   };
 };
 
