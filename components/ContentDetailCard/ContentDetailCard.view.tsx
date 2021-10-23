d3e9c3181554c86df692c0dad5b294fd648d687a
Benjamin <eunvanz@naver.com>
2021-10-23 11:56:27 +0900

    feat: apply ContentModal

--- components/ContentDetailCard/ContentDetailCard.view.tsx
+++ components/ContentDetailCard/ContentDetailCard.view.tsx
@@ -1,102 +1,121 @@
-import { useEffect, useState } from "react";
+import { useEffect, forwardRef } from "react";
 import {
   AddOutlined,
+  CloseOutlined,
   ContentCopyOutlined,
   ModeEditOutlineOutlined,
 } from "@mui/icons-material";
 import {
   Box,
   Typography,
-  Button,
   Chip,
   Card,
   CardMedia,
   CardContent,
   CardActions,
+  IconButton,
 } from "@mui/material";
 import Clipboard from "clipboard";
 import { useRouter } from "next/dist/client/router";
 import { useSnackbar } from "notistack";
 import ROUTES from "~/routes";
 import { Content } from "~/types";
-import ContentModal from "../ContentModal";
 
 export interface ContentDetailCardProps {
   content: Content;
+  onClose?: VoidFunction;
 }
 
-const ContentDetailCard: React.FC<ContentDetailCardProps> = ({ content }) => {
-  const router = useRouter();
+// eslint-disable-next-line
+const ContentDetailCard = forwardRef<HTMLDivElement, ContentDetailCardProps>(
+  ({ content, onClose }, forwardedRef) => {
+    const router = useRouter();
 
-  const { enqueueSnackbar } = useSnackbar();
+    const { enqueueSnackbar } = useSnackbar();
 
-  useEffect(() => {
-    const clipboard = new Clipboard("#copy-to-clipboard");
-    clipboard.on("success", () => {
-      enqueueSnackbar("링크가 복사되었습니다.");
-    });
-    return () => {
-      clipboard.destroy();
-    };
-  }, [enqueueSnackbar]);
+    useEffect(() => {
+      const clipboard = new Clipboard("#copy-to-clipboard");
+      clipboard.on("success", () => {
+        enqueueSnackbar("링크가 복사되었습니다.");
+      });
+      return () => {
+        clipboard.destroy();
+      };
+    }, [enqueueSnackbar]);
 
-  return (
-    <Card sx={{ boxShadow: 20 }}>
-      <CardMedia
-        component="img"
-        image={content.images[content.images.length - 1]?.url}
-        alt={content.title || "untitled"}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {content.title}
-        </Typography>
-        <Typography gutterBottom variant="body2" color="text.secondary">
-          {content.description}
-        </Typography>
-        {!!content.tags.length && (
+    return (
+      <Card sx={{ boxShadow: 20 }} ref={forwardedRef}>
+        <CardMedia
+          component="img"
+          image={content.images[content.images.length - 1]?.url}
+          alt={content.title || "untitled"}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {content.title}
+          </Typography>
+          <Typography gutterBottom variant="body2" color="text.secondary">
+            {content.description}
+          </Typography>
+          {!!content.tags.length && (
+            <Box
+              sx={{
+                my: 2,
+              }}
+            >
+              {content.tags.map((tag) => (
+                <Chip sx={{ mr: 0.5, mb: 0.5 }} key={tag.id} label={tag.name} />
+              ))}
+            </Box>
+          )}
+          <Typography variant="body2" color="text.disabled">
+            zzal.me/{content.path}
+          </Typography>
+        </CardContent>
+        <CardActions>
           <Box
             sx={{
-              my: 2,
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
             }}
           >
-            {content.tags.map((tag) => (
-              <Chip sx={{ mr: 0.5, mb: 0.5 }} key={tag.id} label={tag.name} />
-            ))}
+            <Box>
+              <IconButton
+                aria-label="링크 복사"
+                id="copy-to-clipboard"
+                data-clipboard-text={`https://zzal.me/${content.path}`}
+              >
+                <ContentCopyOutlined />
+              </IconButton>
+              {!content.userId && (
+                <IconButton
+                  aria-label="짤 수정"
+                  size="small"
+                  onClick={() => router.push(`${ROUTES.REGISTRATION}/${content.path}`)}
+                >
+                  <ModeEditOutlineOutlined />
+                </IconButton>
+              )}
+            </Box>
+
+            {onClose ? (
+              <IconButton aria-label="닫기" onClick={onClose}>
+                <CloseOutlined />
+              </IconButton>
+            ) : (
+              <IconButton
+                aria-label="다른 짤 추가"
+                onClick={() => router.push(ROUTES.REGISTRATION__NEW)}
+              >
+                <AddOutlined />
+              </IconButton>
+            )}
           </Box>
-        )}
-        <Typography variant="body2" color="text.disabled">
-          zzal.me/{content.path}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button
-          size="small"
-          endIcon={<ContentCopyOutlined />}
-          id="copy-to-clipboard"
-          data-clipboard-text={`https://zzal.me/${content.path}`}
-        >
-          링크 복사
-        </Button>
-        {!content.userId && (
-          <Button
-            size="small"
-            onClick={() => router.push(`${ROUTES.REGISTRATION}/${content.path}`)}
-            endIcon={<ModeEditOutlineOutlined />}
-          >
-            짤 수정
-          </Button>
-        )}
-        <Button
-          size="small"
-          onClick={() => router.push(ROUTES.REGISTRATION__NEW)}
-          endIcon={<AddOutlined />}
-        >
-          다른 짤 등록
-        </Button>
-      </CardActions>
-    </Card>
-  );
-};
+        </CardActions>
+      </Card>
+    );
+  },
+);
 
 export default ContentDetailCard;
--- components/ContentModal/ContentModal.view.tsx
+++ components/ContentModal/ContentModal.view.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react";
+import { SignalWifiStatusbarConnectedNoInternet4TwoTone } from "@mui/icons-material";
 import { Modal, Box, Fade, Backdrop } from "@mui/material";
 import { Content } from "~/types";
 import ContentDetailCard from "../ContentDetailCard";
@@ -9,6 +11,31 @@ export interface ContentModalProps {
 }
 
 const ContentModal: React.FC<ContentModalProps> = ({ content, isOpen, onClose }) => {
+  const cardRef = useRef<HTMLDivElement>();
+
+  const [boxStyle, setBoxStyle] = useState({
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+  });
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        let boxStyle = {
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+        };
+        if ((cardRef.current?.clientHeight || 0) > window.innerHeight - 24) {
+          boxStyle = {
+            top: "0px",
+            transform: "translate(-50%)",
+          };
+        }
+        setBoxStyle(boxStyle);
+      });
+    }
+  }, [isOpen]);
+
   return (
     <Modal
       open={isOpen}
@@ -16,19 +43,28 @@ const ContentModal: React.FC<ContentModalProps> = ({ content, isOpen, onClose })
       closeAfterTransition
       BackdropComponent={Backdrop}
       BackdropProps={{ timeout: 500 }}
+      sx={{
+        overflow: "auto",
+      }}
     >
       <Fade in={isOpen}>
         <Box
+          ref={cardRef}
           sx={{
             position: "absolute",
-            top: "50%",
             left: "50%",
-            transform: "translate(-50%, -50%)",
             p: 2,
-            maxWidth: "100%",
+            width: {
+              xs: "100%",
+              md: "auto",
+            },
+            "&:focus": {
+              outline: "none",
+            },
+            ...boxStyle,
           }}
         >
-          <ContentDetailCard content={content} />
+          <ContentDetailCard content={content} onClose={onClose} />
         </Box>
       </Fade>
     </Modal>
