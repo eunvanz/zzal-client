0ef892124c07096b8c2a89a21afd8f1791c91b00
Benjamin <eunvanz@naver.com>
2021-12-08 22:22:06 +0900

    feat: apply og image tag to index page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,6 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { DEFAULT_TITLE } from "~/constants/text";
+import {
+  DEFAULT_TITLE,
+  LOGO_IMAGE_HEIGHT,
+  LOGO_IMAGE_TYPE,
+  LOGO_IMAGE_URL,
+  LOGO_IMAGE_WIDTH,
+} from "~/constants/text";
 import Main from "./Main.view";
 import useMainProps from "./useMainProps";
 
@@ -12,6 +18,12 @@ const Home: NextPage = () => {
       <Head>
         <title>{DEFAULT_TITLE}</title>
         <meta name="description" content="손쉽게 짤 링크를 생성하고 공유하세요." />
+        <meta property="og:image" content={LOGO_IMAGE_URL} />
+        <meta property="og:image:url" content={LOGO_IMAGE_URL} />
+        <meta property="og:image:secure_url" content={LOGO_IMAGE_URL} />
+        <meta property="og:image:type" content={LOGO_IMAGE_TYPE} />
+        <meta property="og:image:width" content={LOGO_IMAGE_WIDTH} />
+        <meta property="og:image:height" content={LOGO_IMAGE_HEIGHT} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
