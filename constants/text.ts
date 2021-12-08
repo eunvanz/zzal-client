900361b4105ec6f6bbba2d9057fc36bc7292acf6
Benjamin <eunvanz@naver.com>
2021-12-08 22:20:51 +0900

    feat: apply og image tag to not found page

--- constants/text.ts
+++ constants/text.ts
@@ -1,1 +1,6 @@
 export const DEFAULT_TITLE = "zzal.me - 짤 링크 생성기";
+export const LOGO_IMAGE_URL =
+  "https://s3.ap-northeast-2.amazonaws.com/files.zzal.me/images/zzalme_lthumbnail.png";
+export const LOGO_IMAGE_TYPE = "image/png";
+export const LOGO_IMAGE_WIDTH = "296";
+export const LOGO_IMAGE_HEIGHT = "164";
--- pages/not-found/index.page.tsx
+++ pages/not-found/index.page.tsx
@@ -1,6 +1,12 @@
 import Head from "next/head";
 import BaseLayout from "~/components/BaseLayout";
 import NotFound from "~/components/NotFound";
+import {
+  LOGO_IMAGE_HEIGHT,
+  LOGO_IMAGE_TYPE,
+  LOGO_IMAGE_URL,
+  LOGO_IMAGE_WIDTH,
+} from "~/constants/text";
 
 const NotFoundPage: React.FC<void> = () => {
   return (
@@ -8,6 +14,12 @@ const NotFoundPage: React.FC<void> = () => {
       <Head>
         <title>zzal.me - 등록되지 않은 짤입니다.</title>
         <meta name="og:description" content="짤을 등록 후 사용해주세요." />
+        <meta property="og:image" content={LOGO_IMAGE_URL} />
+        <meta property="og:image:url" content={LOGO_IMAGE_URL} />
+        <meta property="og:image:secure_url" content={LOGO_IMAGE_URL} />
+        <meta property="og:image:type" content={LOGO_IMAGE_TYPE} />
+        <meta property="og:image:width" content={LOGO_IMAGE_WIDTH} />
+        <meta property="og:image:height" content={LOGO_IMAGE_HEIGHT} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
